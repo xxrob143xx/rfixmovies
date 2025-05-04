becf54c1d85ff897d3da69582503ba0d08c0f06f
xxrob143xx <rbaclle111@gmail.com>
2025-05-05 00:56:59 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = '7b509c844ec2f2b2ac7d169885b4971d';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
